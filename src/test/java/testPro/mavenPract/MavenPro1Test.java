bf26add44f784e06f9f2713bcb074ac374377d38
ankurkanish <ankurkanish9@gmail.com>
2020-07-23 23:11:02 +0530

    first commit

---
+++ src/test/java/testPro/mavenPract/MavenPro1Test.java
@@ -0,0 +1,23 @@
+package testPro.mavenPract;
+
+import org.testng.annotations.Test;
+
+public class MavenPro1Test {
+	
+	@Test
+	public void CheckTest()
+	{
+	
+		System.out.println("Test1");
+	
+	}
+	
+	@Test
+	public void Check1Test()
+	{
+	
+		System.out.println("Test2");
+	
+	}
+	
+	}
---
+++ src/test/java/testPro/mavenPract/MavenProTest.java
@@ -0,0 +1,24 @@
+package testPro.mavenPract;
+
+import org.testng.annotations.Test;
+
+public class MavenProTest {
+	
+	
+	@Test
+	public void Check2Test()
+	{
+	
+		System.out.println("Test3");
+	
+	}
+	
+	@Test
+	public void Check3Test()
+	{
+	
+		System.out.println("Test4");
+	
+	}
+
+}
